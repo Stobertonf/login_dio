c264106fdafc1d8ce9f3a6d2b5f8e44fe0e69f11
stobertonf <stoberton.f@lyncas.net>
2023-04-01 17:52:01 -0300

    feat(Lists): Creating List View Horizontal File.

---
+++ lib/pages/lists/list_view_horizontal.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:login_dio_flutter/shared/app_images.dart';
+
+
+class ListViewHorizontal extends StatefulWidget {
+  const ListViewHorizontal({Key? key}) : super(key: key);
+
+  @override
+  State<ListViewHorizontal> createState() => _ListViewHorizontalState();
+}
+
+class _ListViewHorizontalState extends State<ListViewHorizontal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        Expanded(
+          flex: 2,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              Card(
+                elevation: 8,
+                child: Image.asset(
+                  AppImages.paisagem1,
+                ),
+              ),
+              Card(
+                elevation: 8,
+                child: Image.asset(
+                  AppImages.paisagem2,
+                ),
+              ),
+              Card(
+                elevation: 8,
+                child: Image.asset(
+                  AppImages.paisagem3,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(flex: 3, child: Container())
+      ],
+    ));
+  }
+}
