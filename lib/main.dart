ea2f3a80a2dc468bb8b63b84a77d07316a7eb32c
stobertonf <stoberton.f@lyncas.net>
2023-04-11 10:24:51 -0300

    feat(Hive): Starting Hive.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'my_app.dart';
+import 'package:hive/hive.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+ //Coloca o alias pra chamar todas ass funções do path_provider.
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var documentDiretory = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(documentDiretory.path); //Inicianddo o Hive
   runApp(const MyApp());
 }
