1f8879325e3373160563dde8809d38ba357ed609
stobertonf <stoberton.f@lyncas.net>
2023-04-11 22:32:00 -0300

    feat(Hive): Creating Task Hive Page File.

---
+++ lib/pages/hive/tasks_page_hive.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class TaskHivePage extends StatefulWidget {
+  const TaskHivePage({super.key});
+
+  @override
+  State<TaskHivePage> createState() => _TaskHivePageState();
+}
+
+class _TaskHivePageState extends State<TaskHivePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
