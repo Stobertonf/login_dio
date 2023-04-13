6df299372eb03424a8e05aee33379b997413dfe1
stobertonf <stoberton.f@lyncas.net>
2023-04-13 15:02:52 -0300

    feat(SQLite): Creating Task SQLite Page File.

---
+++ lib/pages/tasks/tarefa_sqlite_page.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class TaskSQLitePage extends StatefulWidget {
+  const TaskSQLitePage({super.key});
+
+  @override
+  State<TaskSQLitePage> createState() => _TaskSQLitePageState();
+}
+
+class _TaskSQLitePageState extends State<TaskSQLitePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
