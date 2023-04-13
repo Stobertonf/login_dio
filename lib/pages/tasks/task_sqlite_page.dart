7f4127cf0a36f0b7b59e14a4168d4999de669352
stobertonf <stoberton.f@lyncas.net>
2023-04-13 15:10:01 -0300

    feat(SQLite): Finalizing the SQLite Page File.

---
+++ lib/pages/tasks/task_sqlite_page.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:login_dio_flutter/models/task_sqlite.dart';
+import 'package:login_dio_flutter/repositories/sqlite/task_sqllite_repository.dart';
+
+class TaskSQLitePage extends StatefulWidget {
+  const TaskSQLitePage({super.key});
+
+  @override
+  State<TaskSQLitePage> createState() => _TaskSQLitePageState();
+}
+
+class _TaskSQLitePageState extends State<TaskSQLitePage> {
+ TaskSQLiteRepository taskRepository = TaskSQLiteRepository();
+  var _tarefas = const <TaskSQLiteModel>[];
+  var descricaoContoller = TextEditingController();
+  var apenasNaoConcluidos = false;
+
+  @override
+  void initState() {
+    super.initState();
+    obterTarefas();
+  }
+
+  void obterTarefas() async {
+    _tarefas = await taskRepository.obterDados(apenasNaoConcluidos);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            descricaoContoller.text = "";
+            showDialog(
+                context: context,
+                builder: (BuildContext bc) {
+                  return AlertDialog(
+                    title: const Text("Adicionar tarefa"),
+                    content: TextField(
+                      controller: descricaoContoller,
+                    ),
+                    actions: [
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text("Cancelar")),
+                      TextButton(
+                          onPressed: () async {
+                            await taskRepository.save(TaskSQLiteModel(
+                                0, descricaoContoller.text, false));
+                            Navigator.pop(context);
+                            obterTarefas();
+                            setState(() {});
+                          },
+                          child: const Text("Salvar"))
+                    ],
+                  );
+                });
+          },
+        ),
+        body: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Apenas não concluídos",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Switch(
+                        value: apenasNaoConcluidos,
+                        onChanged: (bool value) {
+                          apenasNaoConcluidos = value;
+                          obterTarefas();
+                        })
+                  ],
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: _tarefas.length,
+                    itemBuilder: (BuildContext bc, int index) {
+                      var tarefa = _tarefas[index];
+                      return Dismissible(
+                        onDismissed: (DismissDirection dismissDirection) async {
+                          taskRepository.delete(tarefa.id);
+                          obterTarefas();
+                        },
+                        key: Key(tarefa.descricao),
+                        child: ListTile(
+                          title: Text(tarefa.descricao),
+                          trailing: Switch(
+                            onChanged: (bool value) async {
+                              tarefa.concluido = value;
+                              taskRepository.update(tarefa);
+                              obterTarefas();
+                            },
+                            value: tarefa.concluido,
+                          ),
+                        ),
+                      );
+                    }),
+              ),
+            ],
+          ),
+        ));
+  }
+}
