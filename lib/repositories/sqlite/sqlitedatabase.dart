26604c98ae92706017b1a4ca3abc0997cf6af21f
stobertonf <stoberton.f@lyncas.net>
2023-04-13 14:04:59 -0300

    feat(sqLite): Creating the SQLite Database.

---
+++ lib/repositories/sqlite/sqlitedatabase.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart' as path;
+
+Map<int, String> scripts = {
+  1: ''' CREATE TABLE task (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          descricao TEXT,
+          concluido INTEGER
+          );'''
+};
+
+class SQLiteDataBase {
+  static Database? db;
+
+  Future<Database>getDataBase() async {
+    if (db == null) {
+      return await startDatabase();
+    } else {
+      return db!;
+    }
+  }
+
+  Future<Database> startDatabase() async {
+    var db = await openDatabase(
+        path.join(await getDatabasesPath(), 'database.db'),
+        version: scripts.length, onCreate: (Database db, int version) async {
+      for (var i = 1; i <= scripts.length; i++) {
+        await db.execute(scripts[i]!);
+        debugPrint(scripts[i]!);
+      }
+    }, onUpgrade: (Database db, int oldVersion, int newVersion) async {
+      for (var i = oldVersion + 1; i <= scripts.length; i++) {
+        await db.execute(scripts[i]!);
+        debugPrint(scripts[i]!);
+      }
+    });
+    return db;
+  }
+}
