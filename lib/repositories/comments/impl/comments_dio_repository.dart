3045b386aa3bdb9334c6c294e0e5ebac72b45765
stobertonf <stoberton.f@lyncas.net>
2023-04-23 12:41:54 -0300

    feat(Ceps): Creating The Comments Dio Repsositoy.

---
+++ lib/repositories/comments/impl/comments_dio_repository.dart
@@ -0,0 +1,16 @@
+import 'package:login_dio_flutter/models/comment_model.dart';
+import 'package:login_dio_flutter/repositories/jsonplaceholder_custon_dio.dart';
+import 'package:login_dio_flutter/repositories/comments/comments_repository.dart';
+
+
+class CommentsDioRepsositoy implements CommentsRepository {
+  @override
+  Future<List<CommentModel>> retornaComentarios(int postId) async {
+    var jsonPlaceHolderCustonDio = JsonPlaceHolderCustonDio();
+    var response =
+        await jsonPlaceHolderCustonDio.dio.get("/posts/$postId/comments");
+    return (response.data as List)
+        .map((e) => CommentModel.fromJson(e))
+        .toList();
+  }
+}
