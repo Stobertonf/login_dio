ab7f1bcc3f2f0e69d3a3be50c79c4a4d515a121e
stobertonf <stoberton.f@lyncas.net>
2023-04-23 12:14:19 -0300

    feat(Ceps): Finishing the cep screen

--- lib/pages/ceps/consulta_cep.dart
+++ lib/pages/ceps/consulta_cep.dart
@@ -1,18 +1,66 @@
-
-
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-
-class ConsultaCep extends StatefulWidget {
-  const ConsultaCep({super.key});
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:login_dio_flutter/models/viacep_model.dart';
+import 'package:login_dio_flutter/repositories/via_cep_repository.dart';
+class ConsultaCEP extends StatefulWidget {
+  const ConsultaCEP({Key? key}) : super(key: key);
 
   @override
-  State<ConsultaCep> createState() => _ConsultaCepState();
+  State<ConsultaCEP> createState() => _ConsultaCEPState();
 }
 
-class _ConsultaCepState extends State<ConsultaCep> {
+class _ConsultaCEPState extends State<ConsultaCEP> {
+  var cepController = TextEditingController(text: "");
+  bool loading = false;
+  var viacepModel = ViaCEPModel();
+  var viaCEPRepository = ViaCepRepository();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+        child: Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          children: [
+           const Text(
+              "Consulta de CEP",
+              style: TextStyle(fontSize: 22),
+            ),
+            TextField(
+              controller: cepController,
+              keyboardType: TextInputType.number,
+              onChanged: (String value) async {
+                var cep = value.replaceAll( RegExp(r'[^0-9]'), '');
+                if (cep.length == 8) {
+                  setState(() {
+                    loading = true;
+                  });
+                  viacepModel = await viaCEPRepository.consultarCEP(cep);
+                }
+                setState(() {
+                  loading = false;
+                });
+              },
+            ),
+         const SizedBox(
+              height: 50,
+            ),
+            Text(
+              viacepModel.logradouro ?? "",
+              style: const TextStyle(fontSize: 22),
+            ),
+            Text(
+              "${viacepModel.localidade ?? ""} - ${viacepModel.uf ?? ""}",
+              style: const TextStyle(fontSize: 22),
+            ),
+            if (loading) const CircularProgressIndicator()
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () async {},
+      ),
+    ));
   }
 }
