3f81239ce4cc7de3c13d0336e507686b3558630c
stobertonf <stoberton.f@lyncas.net>
2023-04-23 12:41:01 -0300

    fixe(Ceps): Fixing Comments Http Repository File.

--- lib/repositories/comments/impl/comments_http_repository.dart
+++ lib/repositories/comments/impl/comments_http_repository.dart
@@ -1,17 +1,19 @@
-
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:login_dio_flutter/models/comment_model.dart';
-import 'package:login_dio_flutter/repositories/jsonplaceholder_custon_dio.dart';
 import 'package:login_dio_flutter/repositories/comments/comments_repository.dart';
 
-
-class CommentsDioRepsositoy implements CommentsRepository {
+class CommentsHttpRepository implements CommentsRepository {
   @override
   Future<List<CommentModel>> retornaComentarios(int postId) async {
-    var jsonPlaceHolderCustonDio = JsonPlaceHolderCustonDio();
-    var response =
-        await jsonPlaceHolderCustonDio.dio.get("/posts/$postId/comments");
-    return (response.data as List)
-        .map((e) => CommentModel.fromJson(e))
-        .toList();
+    var response = await http.get(Uri.parse(
+        "https://jsonplaceholder.typicode.com/posts/$postId/comments"));
+    if (response.statusCode == 200) {
+      var jsonComments = jsonDecode(response.body);
+      return (jsonComments as List)
+          .map((e) => CommentModel.fromJson(e))
+          .toList();
+    }
+    return [];
   }
 }
