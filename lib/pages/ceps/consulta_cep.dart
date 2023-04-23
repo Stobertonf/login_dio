73f8722b37ce13c293d8abaf4145bf91b08dac26
stobertonf <stoberton.f@lyncas.net>
2023-04-23 11:53:08 -0300

    feat(Ceps): Creating The Ceps Page.

---
+++ lib/pages/ceps/consulta_cep.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ConsultaCep extends StatefulWidget {
+  const ConsultaCep({super.key});
+
+  @override
+  State<ConsultaCep> createState() => _ConsultaCepState();
+}
+
+class _ConsultaCepState extends State<ConsultaCep> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
