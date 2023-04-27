ca3ae8a270c04e7311d41c1a388adc0df76c2898
stobertonf <stobertonf@gmail.com>
2023-04-27 15:07:27 -0300

    feat(HTTP): Creating Dados Cadastrais Model file.

---
+++ lib/models/dados_cadastrais_model.dart
@@ -0,0 +1,33 @@
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 0)
+class DadosCadastraisModel extends HiveObject {
+  @HiveField(0)
+  String? nome;
+
+  @HiveField(1)
+  DateTime? dataNascimento;
+
+  @HiveField(2)
+  String? nivelExperiencia;
+
+  @HiveField(3)
+  List<String> linguagens = [];
+
+  @HiveField(4)
+  int? tempoExperiencia;
+
+  @HiveField(5)
+  double? salario;
+
+  DadosCadastraisModel();
+
+  DadosCadastraisModel.vazio() {
+    nome = "";
+    dataNascimento = null;
+    nivelExperiencia = "";
+    linguagens = [];
+    tempoExperiencia = 0;
+    salario = 0;
+  }
+}
