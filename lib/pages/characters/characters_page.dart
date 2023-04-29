af7933552cb8119582f20f23fa3ad2e7f3873894
stobertonf <stobertonf@gmail.com>
2023-04-29 15:17:35 -0300

    fixe(Marvel): Fixing screen structure.

--- lib/pages/characters/characters_page.dart
+++ lib/pages/characters/characters_page.dart
@@ -47,52 +47,63 @@ class _CharactersPagePageState extends State<CharactersPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListView.builder(
-        itemCount:
-            (characters.data == null) || characters.data!.results == null,
-        itemBuilder: (BuildContext context, int index) {},
-      )
-          ? 0
-          : characters.data!.results!.length,
-      itemBuilder: (_, int index) {
-        return Container(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.network(
-                characters.thumbnail!.path! +
-                    "." +
-                    characters.thumbnail!.extension!,
-                width: 150,
-                height: 150,
-              ),
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 8,
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        characters.name!,
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+                itemCount: (characters.data == null ||
+                        characters.data!.results == null)
+                    ? 0
+                    : characters.data!.results!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  var character = characters.data!.results![index];
+                  return Card(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.network(
+                          character.thumbnail!.path! +
+                              "." +
+                              character.thumbnail!.extension!,
+                          width: 150,
+                          height: 150,
                         ),
-                      ),
-                      Text(characters.description),
-                    ],
-                  ),
-                ),
-              ),
-            ],
+                        Expanded(
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 8, horizontal: 8),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  character.name!,
+                                  style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                Text(character.description!),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
           ),
-        );
-      },
-    );
+          !carregando
+              ? ElevatedButton(
+                  onPressed: () {
+                    carregarDados();
+                  },
+                  child: const Text("Carregar mais itens"))
+              : const CircularProgressIndicator()
+        ],
+      ),
+    ));
   }
 }
