00f92337a042ef7dddbfdf9c0f1ed2991daf11fe
stobertonf <stobertonf@gmail.com>
2023-04-29 14:53:47 -0300

    feat(Marvel): Adding initState.

--- lib/pages/characters/characters_page.dart
+++ lib/pages/characters/characters_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:login_dio_flutter/models/characters_model.dart';
+import 'package:login_dio_flutter/repositories/marvel/marvel_repository.dart';
 
 class CharactersPage extends StatefulWidget {
   const CharactersPage({super.key});
@@ -8,8 +10,31 @@ class CharactersPage extends StatefulWidget {
 }
 
 class _CharactersPagePageState extends State<CharactersPage> {
+  int offset = 0;
+  var carregando = false;
+  late MarvelRepository marvelRepository;
+  CharactersModel characters = CharactersModel();
+  ScrollController _scrollController = ScrollController();
+  @override
+  void initState() {
+    _scrollController.addListener(() {
+      var posicaoParaPaginar = _scrollController.position.maxScrollExtent * 0.7;
+      if (_scrollController.position.pixels > posicaoParaPaginar) {
+        print("Carregando página");
+        //Função que vai carregar os dados
+      }
+    });
+    marvelRepository = MarvelRepository();
+    super.initState();
+    //Função que vai carregar os dados
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      child: const Text(
+        'Heróis da Marvel',
+      ),
+    );
   }
 }
