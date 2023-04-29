fa97650eae432111e0277383232aaafd6dfda358
stobertonf <stobertonf@gmail.com>
2023-04-29 13:54:22 -0300

    feat(Marvel): Creating Characters Page File.

---
+++ lib/pages/characters/characters_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CharactersPage extends StatefulWidget {
+  const CharactersPage({super.key});
+
+  @override
+  State<CharactersPage> createState() => _CharactersPagePageState();
+}
+
+class _CharactersPagePageState extends State<CharactersPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
